ac1f0e4faeb169a6aad7fb03822667b72ffe383e
Ubuntu <ubuntu@ip-172-31-47-246.ec2.internal>
2018-10-04 18:57:37 +0000

    added the word chagne

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world! You’re at the home index")
+    return HttpResponse("Hello, world! You’re at the home index Chnage")
 
 # Create your views here.
 
